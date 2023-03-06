fd1d64a4823a4b377273242b97b50d79da50f33b
evalldore <evalldore@gmail.com>
2023-03-06 05:50:57 -0500

    mid read fix, going insane pi C FINI FUCK LA FRANCINETTE

--- get_next_line.c
+++ get_next_line.c
@@ -6,39 +6,38 @@
 /*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 17:54:45 by evallee-          #+#    #+#             */
-/*   Updated: 2023/03/06 03:25:48 by niceguy          ###   ########.fr       */
+/*   Updated: 2023/03/06 05:41:26 by niceguy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 #include "get_next_line.h"
 
-static char	*check_chunk(char	*chunk)
+static char	*make_chunk(void)
 {
 	size_t	i;
+	char	*chunk;
 
 	i = 0;
-	if (!chunk)
-	{
-		chunk = malloc(BUFFER_SIZE + 1);
-		while (i <= BUFFER_SIZE)
-			chunk[i++] = '\0';
-	}
+	chunk = malloc(BUFFER_SIZE + 1);
+	while (i <= BUFFER_SIZE)
+		chunk[i++] = '\0';
 	return (chunk);
 }
 
-static void	read_file(t_fdstate *fdstate)
+static ssize_t	read_file(t_fdstate *fdstate)
 {
 	char	*chunk;
 	char	*new;
 	ssize_t	bytes_read;
 
 	bytes_read = 1;
-	chunk = NULL;
+	chunk = make_chunk();
 	while (bytes_read > 0)
 	{
-		chunk = check_chunk(chunk);
 		bytes_read = read(fdstate->fd, chunk, BUFFER_SIZE);
+		if (bytes_read < 0)
+			break ;
 		chunk[bytes_read] = '\0';
 		new = ft_strjoin(fdstate->buff, chunk);
 		free(fdstate->buff);
@@ -47,6 +46,7 @@ static void	read_file(t_fdstate *fdstate)
 			break ;
 	}
 	free(chunk);
+	return (bytes_read);
 }
 
 t_fdstate	*new_state(int fd)
@@ -88,15 +88,14 @@ char	*get_next_line(int fd)
 		return (NULL);
 	if (!fdstate)
 		fdstate = new_state(fd);
-	read_file(fdstate);
-	line = make_line(fdstate, &fdstate->buff[fdstate->buff_pos]);
-	if (line)
-		return (line);
-	else
+	if (read_file(fdstate) >= 0)
 	{
-		free(fdstate->buff);
-		free(fdstate);
-		fdstate = NULL;
-		return (NULL);
+		line = make_line(fdstate, &fdstate->buff[fdstate->buff_pos]);
+		if (line)
+			return (line);
 	}
+	free(fdstate->buff);
+	free(fdstate);
+	fdstate = NULL;
+	return (NULL);
 }
--- gnl.c
+++ gnl.c
@@ -6,7 +6,7 @@
 /*   By: niceguy <niceguy@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 00:01:43 by niceguy           #+#    #+#             */
-/*   Updated: 2023/03/06 00:21:05 by niceguy          ###   ########.fr       */
+/*   Updated: 2023/03/06 05:28:25 by niceguy          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,34 @@
 int	main(int argc, char **argv)
 {
 	int		fd;
-	int		ln;
+	//int		ln;
 	printf("BUFFER_SIZE: %d\n", BUFFER_SIZE);
-	ln = 0;
+	//ln = 0;
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
 		if (fd > -1)
 		{
-			printf("line(%d) : %s", ln++, get_next_line(fd));
-			printf("line(%d) : %s", ln++, get_next_line(fd));
-			printf("line(%d) : %s", ln++, get_next_line(fd));
-			printf("\n");
-			//printf("line(%d) : %s", ln++, get_next_line(fd));
-			//printf("line(%d) : %s", ln++, get_next_line(fd));
+
+			printf("%s", get_next_line(fd));
+			printf("%s", get_next_line(fd));
+			if (BUFFER_SIZE > 100) {
+				char *temp;
+				do {
+					temp = get_next_line(fd);
+					free(temp);
+				} while (temp != NULL);
+			}
+			printf("%s", get_next_line(fd));
 		}
+			/*printf("line(%d) : %s", ln++, get_next_line(fd));
+			printf("line(%d) : %s", ln++, get_next_line(fd));
+			printf("line(%d) : %s", ln++, get_next_line(fd));
+			printf("line(%d) : %s", ln++, get_next_line(fd));
+			printf("line(%d) : %s", ln++, get_next_line(fd));
+			printf("\n");*/
+			//printf("line(%d) : %s", ln++, get_next_line(fd));
+			//printf("line(%d) : %s", ln++, get_next_line(fd));
 	}
 	return (0);
 }
